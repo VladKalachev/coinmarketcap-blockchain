7415ceee9d765f859fe7d2708c44e54220a15407
Vlad <vlvete1990@yandex.ru>
2022-05-17 21:52:55 +0300

    chore: added Header component

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+
+const styles = {
+  navLink: `text-white flex mx-[10px]`,
+  badge: `rounded-full bg-blue-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-4`,
+  navItem: `relative mr-1 cursor-pointer hover:opacity-60`,
+  nav: `flex justify-center items-center gap-[20px]`,
+  header: `bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]`,
+  headerWrapper: `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
+  inputContainer: `flex items-center justify-center p-2 rounded bg-[#171924]`,
+  input: `bg-transparent outline-none text-white w-70 ml-3`,
+  cursorPointer: `mr-5 cursor-pointer`,
+}
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <Image
+        alt="logo"
+        src="https://s2.coinmarketcap.com/static/cloud/img/coinmarketcap_white_1.svg"
+        width={220}
+        height={220}
+      />
+    </div>
+  )
+}
+
+export default Header
--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['encrypted-tbn0.gstatic.com', 'cdn.sanity.io'],
+  },
+}
+
+module.exports = {
+  images: {
+    domains: [
+      's2.coinmarketcap.com',
+      's3.coinmarketcap.com',
+      'cdn.sanity.io',
+      'encrypted-tbn0.gstatic.com',
+    ],
+  },
+  nextConfig,
 }
