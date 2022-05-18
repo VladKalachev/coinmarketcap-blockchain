98729970ce0a56551ae5f32eb5f254d6892160a0
Vlad <vlvete1990@yandex.ru>
2022-05-18 21:31:26 +0300

    chore: added moralis

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Search from '../assets/svg/search'
+import { ConnectButton } from 'web3uikit'
 
 const styles = {
   navLink: `text-white flex mx-[10px]`,
@@ -63,7 +64,7 @@ const Header = () => {
         </nav>
 
         <div className="flex items-center">
-          {/* <ConnectButton /> */}
+          <ConnectButton />
           <div className={styles.inputContainer}>
             <Search />
             <input className={styles.input} placeholder="Search" />
