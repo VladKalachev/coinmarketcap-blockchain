8b3d38ca8e11acae192b65b32323fba651a3c5f4
Vlad <vlvete1990@yandex.ru>
2022-05-19 22:15:02 +0300

    chore: added getTopTen

---
+++ pages/api/getTopTen.ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const getData = async () => {
+    const response = await fetch(
+      `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${process.env.NEXT_BACKEND_CMC_API_KEY}`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: '*/*',
+        },
+      }
+    )
+
+    const data = await response.json()
+
+    res.status(200).json({ data })
+  }
+
+  getData()
+}
