1212a997499a735181489c68e28c8deee43c9e0f
Vlad <vlvete1990@yandex.ru>
2022-05-19 21:52:43 +0300

    chore: added Trending

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
-import Header from '../components/Header';
+import Header from '../components/Header'
+import Trending from '../components/Trending'
 
 const Home: NextPage = () => {
   return (
-    <div className='min-h-screen'>
+    <div className="min-h-screen">
       <Header />
-      <div className='mt-10'></div>
-      <div className='mt-20'></div>
+      <div className="mt-10"></div>
+      <Trending />
+      <div className="mt-20"></div>
     </div>
   )
 }
