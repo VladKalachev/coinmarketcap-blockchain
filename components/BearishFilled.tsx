334485ce9d0d5545ddaa9790acf582779a61ba4c
Vlad <vlvete1990@yandex.ru>
2022-05-26 18:43:46 +0300

    chore: BearishFilled

---
+++ components/BearishFilled.tsx
@@ -0,0 +1,12 @@
+import ChevronDown from '../assets/svg/chevronDown'
+
+const BearishFilled = () => {
+  return (
+    <div>
+      <ChevronDown fill={'#fff'} />
+      <small>Bearish</small>
+    </div>
+  )
+}
+
+export default BearishFilled
