e528cb1672908c82deaf68e5b89774dba77bc116
Vlad <vlvete1990@yandex.ru>
2022-05-27 18:30:54 +0300

    chore: BullishFilled

---
+++ components/BullishFilled.tsx
@@ -0,0 +1,12 @@
+import ChevronUp from '../assets/svg/chevronUp'
+
+const BullishFilled = () => {
+  return (
+    <div>
+      <ChevronUp fill={'#fff'} />
+      <small>Bullish</small>
+    </div>
+  )
+}
+
+export default BullishFilled
