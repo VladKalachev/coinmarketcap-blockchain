54405e536b8caed73a460250f0894aef65345d7f
Vlad <vlvete1990@yandex.ru>
2022-06-09 20:21:52 +0300

    chore: CMCTableTitle

---
+++ components/cmc-table/CMCTableTitle.tsx
@@ -0,0 +1,38 @@
+import ChevronDown from '../../assets/svg/chevronDown'
+import Info from '../../assets/svg/info'
+
+const CMCTableTitle = () => {
+  return (
+    <tbody>
+      <tr>
+        <th></th>
+        <th>
+          <b>#</b>
+          <ChevronDown fill={'#fff'} />
+        </th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>24h %</th>
+        <th>7d %</th>
+        <th>
+          <div className="flex">
+            <p>Market Cap</p> <Info />
+          </div>
+        </th>
+        <th>
+          <div className="flex">
+            <p>Volume(24h)</p> <Info />
+          </div>
+        </th>
+        <th>
+          <div className="flex">
+            <p>Circulating Supply</p> <Info />
+          </div>
+        </th>
+        <th>Last 7 days</th>
+      </tr>
+    </tbody>
+  )
+}
+
+export default CMCTableTitle
